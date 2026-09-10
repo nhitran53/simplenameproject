e4c6c6af014c8b631e5257256ed24379eb642cd4
Nhi Tran <trannhatuyennhi@gmail.com>
2026-09-10 16:59:50 -0500

    Change text size

--- lib/main.dart
+++ lib/main.dart
@@ -104,7 +104,12 @@ class _MyHomePageState extends State<MyHomePage> {
           // wireframe for each widget.
           mainAxisAlignment: .center,
           children: [
-            const Text('Nhi has pushed the button this many times:'),
+            const Text('Nhi has pushed the button this many times:',
+              style: TextStyle(
+              fontSize: 124.0, // Adjust this number to make it bigger
+              fontWeight: FontWeight.bold, // Optional: makes it bold too
+              ),
+            ),
             Text(
               '$_counter',
               style: Theme.of(context).textTheme.headlineMedium,
